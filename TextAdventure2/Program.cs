151891eb212326ed4d10f17678f943b09d58be05
ffaulkner <forrestfaulkner1@gmail.com>
2019-07-26 18:52:38 -0500

    Warning for Brandon and puzzles 3-4 added

--- TextAdventure2/Program.cs
+++ TextAdventure2/Program.cs
@@ -5,6 +5,7 @@ namespace TextAdventure2
 {
     class Program
     {
+        //If your name is Brandon, stop reading. for a while and run this first. Any farther and you'll spoil puzzles. I'm working on a way to store the secret stuff in a seperate file so you cant see them, but for now just don't look.
         static void Main(string[] args)
         {
             WriteLine("This is a puzzle game, and it probably won't be easy.");
@@ -32,15 +33,23 @@ namespace TextAdventure2
             int destination;
             do
             {
-                input = Console.ReadLine();
+                input = Console.ReadLine().ToLower();
                 switch (input)
                 {
                     case "continue":
                         destination = 2;
                         validInput = true;
                         break;
-                    case "Brutus":
+                    case "brutus":
                         destination = 3;
+                        validInput = true;
+                        break;
+                    case "batterystaple":
+                        destination = 4;
+                        validInput = true;
+                        break;
+                    case "fliesrtasty":
+                        destination = 5;
                         validInput = true;
                         break;
                     default:
@@ -48,30 +57,28 @@ namespace TextAdventure2
                         break;
                 }
             } while (!validInput);
-            Destination:
-            Console.Clear();
-            switch (destination)
+            bool end = false;
+            while (!end)
             {
-                case 2:
-                    Puzzle2();
-                    break;
-                default:
-                    goto Finish;
+                Console.Clear();
+                switch (destination)
+                {
+                    case 2:
+                        Puzzle2();
+                        break;
+                    case 3:
+                        Puzzle3();
+                        break;
+                    case 4:
+                        Puzzle4();
+                        break;
+                    default:
+                        end = true;
+                        break;
+                }
+                destination++;
             }
-            destination++;
-            goto Destination;
-            Finish:
             WriteLine("Congrats! You finished my game. It'll probably get longer over time, so remember the last password you used and you can skip to the new stuff later.");
-        }
-        static void Puzzle2()
-        {
-            string input;
-            do
-            {
-                WriteLine("So, this one won't be as much of a giveaway. You might have to brute force it a bit. Here's your hint: Euxwxv.");
-
-                input = Console.ReadLine();
-            } while (input != "Brutus");
         }
         static void WriteLine(string input)
         {
@@ -82,5 +89,36 @@ namespace TextAdventure2
             }
             Console.Write("\n");
         }
+        static void Puzzle2()
+        {
+            string input;
+            do
+            {
+                WriteLine("So, this one won't be as much of a giveaway. You might have to brute force it a bit. Here's your hint: Euxwxv.");
+
+                input = Console.ReadLine().ToLower();
+            } while (input != "brutus");
+        }
+        static void Puzzle3()
+        {
+            string input;
+            do
+            {
+                WriteLine("It looks like someone hacked into this, you'll need to use the admin password to lock them out.\nI can't give it to you directly, they could intercept it.\nAll I can give you is this: You already have the key, Zfok jstjqhlv jj vtnlfisLnsqcy.");
+
+                input = Console.ReadLine().ToLower();
+            } while (input != "batterystaple");
+        }
+        static void Puzzle4()
+        {
+            string input;
+            do
+            {
+                WriteLine("You know, we actually taught spiders to count once, they loved this set of numbers for some reason: 6 12 9 5 19 18 20 1 19 20 25");
+
+                input = Console.ReadLine().ToLower();
+            } while (input != "fliesrtasty");
+        }
+
     }
 }
